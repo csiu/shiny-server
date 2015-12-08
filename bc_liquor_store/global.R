f2678b4231feaf88fe3dcd0262b024ca597e8512
csiu <celia.sy.siu@gmail.com>
2015-12-08 09:44:37 -0800

    add to coolplot

--- bc_liquor_store/global.R
+++ bc_liquor_store/global.R
@@ -1,5 +1,7 @@
 library(ggplot2)
+library(plyr)
 library(dplyr)
+#library(cowplot)
 
 bcl <- read.csv("data/bcl-data.csv", stringsAsFactors = FALSE) %>%
   tbl_df()
--- bc_liquor_store/server.R
+++ bc_liquor_store/server.R
@@ -24,8 +24,25 @@ function(input, output, session) {
     if (is.null(filtered())) {
       return()
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+    g1 <- ggplot(filtered(), aes(x = Alcohol_Content,
+                                 fill = Type)) +
+      geom_histogram(binwidth = 1, origin = 0.5)
+
+    g2 <- filtered() %>%
+      group_by(Type, Alcohol_Content) %>%
+      summarise(count = n()) %>%
+      mutate(Alcohol_Content = round(Alcohol_Content, 1)) %>%
+      ggplot(aes(x = Alcohol_Content, y = count, fill = Type)) +
+      geom_point(subset = .(Type == "WINE"),
+                 pch=21, size=4) +
+      geom_point(subset = .(Type == "SPIRITS"),
+                 pch=21, size=4) +
+      geom_point(subset = .(Type == "BEER"),
+                 pch=21, size=4) +
+      geom_point(subset = .(Type == "REFRESHMENT"),
+                 pch=21, size=4)
+
+    cowplot::plot_grid(g1, g2, ncol = 1, align = "v")
   })
 
   output$results <- renderDataTable({
