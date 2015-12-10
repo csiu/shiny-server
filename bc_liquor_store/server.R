44c491183ddac8fb309c464c8138c0f50168426d
csiu <celia.sy.siu@gmail.com>
2015-12-09 23:45:23 -0800

    comment out what i don't immediately need

--- bc_liquor_store/server.R
+++ bc_liquor_store/server.R
@@ -1,11 +1,24 @@
 library(shiny)
 
 function(input, output, session) {
-  output$countryOutput <- renderUI({
-    selectInput("countryInput", "Country",
-                sort(unique(bcl$Country)),
-                selected = "CANADA")
-  })
+#   output$countryOutput <- renderUI({
+#     selectInput("countryInput", "Country",
+#                 sort(unique(bcl$Country)),
+#                 selected = "CANADA")
+#   })
+#
+#   filtered_old <- reactive({
+#     if (is.null(input$countryInput)) {
+#       return(NULL)
+#     }
+#
+#     bcl %>%
+#       filter(Price >= input$priceInput[1],
+#              Price <= input$priceInput[2],
+#              Type == input$typeInput,
+#              Country == input$countryInput
+#       )
+#   })
 
   filtered <- reactive({
     if (is.null(input$countryInput)) {
@@ -20,31 +33,36 @@ function(input, output, session) {
       )
   })
 
-  output$coolplot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
-    g1 <- ggplot(filtered(), aes(x = Alcohol_Content,
-                                 fill = Type)) +
-      geom_histogram(binwidth = 1, origin = 0.5)
-
-    g2 <- filtered() %>%
-      group_by(Type, Alcohol_Content) %>%
-      summarise(count = n()) %>%
-      mutate(Alcohol_Content = round(Alcohol_Content, 1)) %>%
-      ggplot(aes(x = Alcohol_Content, y = count, fill = Type)) +
-      geom_point(subset = .(Type == "WINE"),
-                 pch=21, size=4) +
-      geom_point(subset = .(Type == "SPIRITS"),
-                 pch=21, size=4) +
-      geom_point(subset = .(Type == "BEER"),
-                 pch=21, size=4) +
-      geom_point(subset = .(Type == "REFRESHMENT"),
-                 pch=21, size=4)
-
-    cowplot::plot_grid(g1, g2, ncol = 1, align = "v")
   })
 
+#   output$coolplot <- renderPlot({
+#     if (is.null(filtered())) {
+#       return()
+#     }
+#     g1 <- ggplot(filtered(), aes(x = Alcohol_Content,
+#                                  fill = Type)) +
+#       geom_histogram(binwidth = 1, origin = 0.5)
+#
+#     g2 <- filtered() %>%
+#       group_by(Type, Alcohol_Content) %>%
+#       summarise(count = n()) %>%
+#       mutate(Alcohol_Content = round(Alcohol_Content, 1)) %>%
+#       ggplot(aes(x = Alcohol_Content, y = count, fill = Type)) +
+#       geom_point(subset = .(Type == "WINE"),
+#                  pch=21, size=4) +
+#       geom_point(subset = .(Type == "SPIRITS"),
+#                  pch=21, size=4) +
+#       geom_point(subset = .(Type == "BEER"),
+#                  pch=21, size=4) +
+#       geom_point(subset = .(Type == "REFRESHMENT"),
+#                  pch=21, size=4)
+#
+#     cowplot::plot_grid(g1, g2, ncol = 1, align = "v")
+#   })
+#
   output$results <- renderDataTable({
     filtered()
   })
--- bc_liquor_store/ui.R
+++ bc_liquor_store/ui.R
@@ -5,29 +5,29 @@ fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      helpText("Hello world"),
+      #helpText("Hello world"),
       sliderInput("priceInput", "Price",
-                  0, 100, c(25, 40), pre = "$"),
-      checkboxGroupInput("typeInput", "Product type",
-                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                   selected = "WINE"),
-      uiOutput("countryOutput"),
-      #submitButton("Submit"),
-      hr(),
-      sliderInput("sweetnessInput", "Sweetness",
-                  0, 10, c(0, 10)),
-      checkboxInput("sweetnessNAInput", 'Show "NA"',
-                    value = TRUE),
-      hr(),
-      radioButtons("colorInput", "Color",
-                   choices = c("A", "B", "C"),
-                   selected = "A")
+                  0, 100, c(0, 15), pre = "$")
+#       checkboxGroupInput("typeInput", "Product type",
+#                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+#                    selected = "WINE"),
+#       uiOutput("countryOutput"),
+#       #submitButton("Submit"),
+#       hr(),
+#       sliderInput("sweetnessInput", "Sweetness",
+#                   0, 10, c(0, 10)),
+#       checkboxInput("sweetnessNAInput", 'Show "NA"',
+#                     value = TRUE),
+#       hr(),
+#       radioButtons("colorInput", "Color",
+#                    choices = c("A", "B", "C"),
+#                    selected = "A")
     ),
 
     mainPanel(
       tabsetPanel(
-        tabPanel("Coolplot", plotOutput("coolplot")),
-        tabPanel("TODO")
+#         tabPanel("Coolplot", plotOutput("coolplot")),
+#         tabPanel("TODO")
       ),
       br(), br(),
       dataTableOutput("results")
