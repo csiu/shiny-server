8bef341f7f01c8dc21bbdaecd639927c8b39f9c4
csiu <celia.sy.siu@gmail.com>
2015-12-15 21:39:54 -0800

    enable conditionalPanel

--- bc_liquor_store/ui.R
+++ bc_liquor_store/ui.R
@@ -9,25 +9,39 @@ fluidPage(
                   0, 100, c(0, 15), pre = "$"),
       sliderInput("sweetnessInput", "Sweetness",
                   0, 10, c(8, 10)),
-      radioButtons("typeInput", "Product type",
-                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                    selected = "WINE"),
       hr(),
       h3("Specific plot options"),
-      h4("Budget Drinks"),
-      selectInput("viridisInput", 'Viridis Theme',
-                  choices = c("A", "B", "C", "D"),
-                  selected = "D"),
-      br(),
-      h4("Cost of Subtypes"),
-      numericInput("minPriceInput", "Min Cost ($)", value = 0),
-      numericInput("maxPriceInput", "Max Cost ($)", value = 30250),
-      br(),
-      h4("Alcohol Content and Sweetness"),
-      p("Note: These changes are also reflected in the recommendation list."),
-      checkboxGroupInput("multitypeInput", "Products selected",
-                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                   selected = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"))
+      conditionalPanel(
+        condition="input.conditionedPanels == 'Budget Drinks'",
+        h4("Budget Drinks"),
+        selectInput("viridisInput", 'Viridis Theme',
+                    choices = c("A", "B", "C", "D"),
+                    selected = "D")
+      ),
+      conditionalPanel(
+        condition="input.conditionedPanels == 'Sweetness of Subtypes'",
+        radioButtons("typeInput", "Product type",
+                     choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                     selected = "WINE")
+      ),
+      conditionalPanel(
+        condition="input.conditionedPanels == 'Cost of Subtypes'",
+        radioButtons("typeInput", "Product type",
+                     choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                     selected = "WINE"),
+        br(),
+        h4("Cost of Subtypes"),
+        numericInput("minPriceInput", "Min Cost ($)", value = 0),
+        numericInput("maxPriceInput", "Max Cost ($)", value = 30250)
+      ),
+      conditionalPanel(
+        condition="input.conditionedPanels == 'Alcohol content and Sweetness'",
+        h4("Alcohol Content and Sweetness"),
+        p("Note: These changes are also reflected in the recommendation list."),
+        checkboxGroupInput("multitypeInput", "Products selected",
+                           choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                           selected = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"))
+      )
     ),
 
     mainPanel(
@@ -47,7 +61,8 @@ fluidPage(
         tabPanel("Alcohol content and Sweetness",
                  br(),
                  h4(textOutput("summaryfiltered_withMultiTypes")),
-                 plotOutput("alcoholpersweetness"))
+                 plotOutput("alcoholpersweetness")),
+        id = "conditionedPanels"
       ),
       br(),
       br(),
